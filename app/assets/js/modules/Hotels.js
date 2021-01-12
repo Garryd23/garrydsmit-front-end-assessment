b81a0beacf4d2434e6903c56c290d3a91ff4b820
Garryd <garrydsmit@gmail.com>
2021-01-12 17:13:17 +0200

    Completed Switching between tabs functionality

--- app/assets/js/modules/Hotels.js
+++ app/assets/js/modules/Hotels.js
@@ -1,30 +1,52 @@
-class Hotels {
+class Tabs {
   constructor() {
-    this.hotelTab = document.querySelector(".hotels-tab");
-    this.hotelContent = document.querySelector(".hotel-content");
+    // this.hotelTab = document.querySelector(".hotels-tab");
+    // this.hotelContent = document.querySelector(".hotel-content");
+
+    // this.flightsTab = document.querySelector(".flights-tab");
+    // this.flightsContent = document.querySelector(".flight-content");
+
+    // this.comboTab = document.querySelector(".combo-tab");
+    // this.comboContent = document.querySelector(".combo-content");
+
+    // this.holidaysTab = document.querySelector(".holiday-tab");
+    // this.holidaysContent = document.querySelector(".holidays-content");
+    this.tabs = document.querySelectorAll(".tabbed-box__tabs--links");
     this.events();
   }
 
   events() {
-    this.hotelTab.addEventListener("click", () => this.toggleHotelContent());
+    this.toggleContent();
+
+    // this.hotelTab.addEventListener("click", () => this.toggleContent());
   }
 
-  toggleHotelContent() {
-    for (var content of this.hotelContent.parentNode.querySelectorAll(".is-current")) {
-      if (content !== this.hotelContent) {
-        content.classList.toggle("is-current");
-        content.style.display = "none";
-        this.hotelContent.classList.toggle("is-current");
+  toggleContent() {
+    console.log(this.tabs);
+    for (const tab of this.tabs) {
+      tab.addEventListener("click", (e) => {
+        console.log(`${e.target} for: ${e.target.getAttribute("content")}`);
+        let clickedTab = e.target;
+        let content = document.querySelector(clickedTab.getAttribute("content"));
+        console.log(content);
+        for (var contents of content.parentNode.querySelectorAll(".is-current")) {
+          if (contents !== content) {
+            contents.classList.toggle("is-current");
+            contents.classList.toggle("is-hidden");
+            content.classList.toggle("is-current");
+            content.classList.toggle("is-hidden");
 
-        for (var tab of this.hotelTab.parentNode.querySelectorAll(".is-active")) {
-          if (tab !== this.hotelTab) {
-            tab.classList.toggle("is-active");
-            this.hotelTab.classList.toggle("is-active");
+            for (var tabs of clickedTab.parentNode.querySelectorAll(".is-active")) {
+              if (tabs !== clickedTab) {
+                tabs.classList.toggle("is-active");
+                clickedTab.classList.toggle("is-active");
+              }
+            }
           }
         }
-      }
+      });
     }
   }
 }
 
-export default Hotels;
+export default Tabs;
